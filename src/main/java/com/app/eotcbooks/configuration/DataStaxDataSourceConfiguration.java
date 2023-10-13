202c59ca25f6fe5cadb604669284a8b14acb8eb5
mengstabketemaw <mengstabrealk28@gmail.com>
2023-10-13 12:20:02 +0300

    initial commit

---
+++ src/main/java/com/app/eotcbooks/configuration/DataStaxDataSourceConfiguration.java
@@ -0,0 +1,12 @@
+package com.app.eotcbooks.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataStaxDataSourceConfiguration {
+
+
+    @Bean
+    javax.sql.DataSource
+}
