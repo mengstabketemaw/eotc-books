4da682b5b727b1622108f989e5443dc778486004
mengstabketemaw <mengstabrealk28@gmail.com>
2023-10-13 13:57:52 +0300

    configure the database

--- src/main/java/com/app/eotcbooks/configuration/DataStaxDataSourceConfiguration.java
+++ src/main/java/com/app/eotcbooks/configuration/DataStaxDataSourceConfiguration.java
@@ -1,12 +1,21 @@
 package com.app.eotcbooks.configuration;
 
+import com.datastax.oss.driver.api.core.CqlSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
 
 @Configuration
 public class DataStaxDataSourceConfiguration {
-
-
     @Bean
-    javax.sql.DataSource
+    public CqlSession session() throws IOException {
+        ClassPathResource resource = new ClassPathResource("secure-connect-eotc-books.zip");
+        return CqlSession.builder()
+                .withCloudSecureConnectBundle(resource.getInputStream())
+                .withAuthCredentials("OiaNAjcZkaEoyozpdwGaimUt","+2AN0,pAMZTb6F8,boCeP98rmbAacl2DzvrmZg,+7wd3Bp,CLtGfXlXtN82lDbm5EzDy+6XjbwTLp35vv6sIanspH,Sxy7ZDGEfuiQf.x_9n-9iUNlkZ7AJ8msJJI,P8")
+                .build();
+
+    }
 }
