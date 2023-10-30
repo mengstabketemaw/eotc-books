1b069f23492ced30d59956fbe422a5f645a7cea6
mengstabketemaw <mengstabrealk28@gmail.com>
2023-10-30 15:57:30 +0300

    Added search functionality.

--- src/main/java/com/app/eotcbooks/controller/ApiController.java
+++ src/main/java/com/app/eotcbooks/controller/ApiController.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -17,5 +19,10 @@ public class ApiController {
     @GetMapping("/books")
     public Page<Book> getBooks(Pageable page){
         return bookService.getBooks(page);
+    }
+
+    @GetMapping("/search")
+    public List<Book> search(@RequestParam String key){
+        return bookService.searchBook(key);
     }
 }
--- src/main/java/com/app/eotcbooks/repository/BookRepository.java
+++ src/main/java/com/app/eotcbooks/repository/BookRepository.java
@@ -2,7 +2,15 @@ package com.app.eotcbooks.repository;
 
 import com.app.eotcbooks.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 public interface BookRepository extends JpaRepository<Book, String> {
+    @Query("""
+            select e from EOTC_BOOKS e
+            where upper(e.name) like upper(concat('%', ?1, '%')) or upper(e.category) like upper(concat('%', ?2, '%'))""")
+    List<Book> search(String name, String category);
+
 }
