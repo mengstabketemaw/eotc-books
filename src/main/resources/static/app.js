cdd0aa856983c978df24777306ea1c6d065fe1f3
mengstabketemaw <mengstabrealk28@gmail.com>
2023-11-28 14:02:12 +0300

    Make the page more responsive.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,13 +1,15 @@
 let pageNumber = 0;
 function loadBook() {
-    $('#loading-place-holder').show();
+
+    $.LoadingOverlay("show");
+
     $('#load-more').hide();
 
     $.ajax({
         url: `/api/books?page=${pageNumber++}&size=50`,
         method: 'GET',
         success: function (data) {
-            $('#loading-place-holder').hide();
+            $.LoadingOverlay("hide");
             $('#load-more').show();
 
 
@@ -17,27 +19,31 @@ function loadBook() {
                 let cover = book.pages[0] || "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png";
 
                 //Create pages of the book
-                const pages = $('<div class="visually-hidden">');
+                const pages = $('#book-galleries');
+
+                book.pages.shift();//Remove the first cover
+
                 book.pages.forEach(url=>{
                     const pageImage = `<a href=${url} data-fancybox=${book.id} data-caption=${book.name}><img src=${url} loading="lazy" alt=${book.name} onerror="this.error=null; this.src='https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png'"></a>`;
                     pages.append(pageImage);
                 });
 
+
                 const bookCard =
-                    `<div class="shadow-none p-1 m-2 bg-body-tertiary rounded" style="height: 250px; width: 200px">
-                        <div class="" style="">
-                            <a  href=${cover} data-fancybox=${book.id} data-caption=${book.name} style="display:block; height:128px; overflow:hidden">
-                                <img src=${cover} class="img-thumbnail lazy" loading="lazy" alt=${book.name} style="width: 192px">
-                            </a>
+                `<div class="col book-card shadow-none p-1 m-2 bg-body-tertiary rounded" style="width: auto">
+                    <div class="" style="">
+                        <a  href=${cover} data-fancybox=${book.id} data-caption=${book.name} style="display:block; height:128px; overflow:hidden">
+                            <img src=${cover} class="img-thumbnail lazy" loading="lazy" alt=${book.name}>
+                        </a>
+                    </div>
+                    <div class="p-2 d-flex flex-column text-capitalize text-nowrap ">
+                        <h6 class="overflow-hidden">${book.name} </h6>
+                        <span class="fs-6 overflow-hidden">${book.category}</span>
+                        <div class="d-flex flex-row-reverse mt-2">
+                            <a href=${book.address} role="button" target="_blank" style="background-color: #AA8661" class="mt-3 btn btn-sm float-right text-light" >Download</a>
                         </div>
-                        <div class="p-2 d-flex flex-column text-capitalize text-nowrap ">
-                            <h6 class="overflow-hidden">${book.name} </h6>
-                            <span class="fs-6 overflow-hidden">${book.category}</span>
-                            <div class="d-flex flex-row-reverse mt-2">
-                                <a href=${book.address} role="button" target="_blank" style="background-color: #AA8661" class="mt-3 btn btn-sm float-right text-light" >Download</a>
-                            </div>
-                        </div>
-                    </div>`;
+                    </div>
+                </div>`;
                 bookListContainer.append(bookCard);
                 bookListContainer.append(pages);
             });
@@ -52,7 +58,7 @@ function loadBook() {
 
 function search_books(key) {
 
-    $('#loading-place-holder').show();
+    $.LoadingOverlay("show");
     $('#load-more').hide();
     $('#bookList').addClass('visually-hidden');
     $('#book-not-found').addClass('visually-hidden');
@@ -62,7 +68,7 @@ function search_books(key) {
         url: `/api/search?key=${key}`,
         method: 'GET',
         success: function (data) {
-            $('#loading-place-holder').hide();
+            $.LoadingOverlay("hide");
             const bookListContainer = $('#searchResult');
             if(!data.length){
                 $('#book-not-found').removeClass('visually-hidden');
